bcb89e0cfd8820b2564e03df977aea08586193d4
Martin Petr <git@bodkan.net>
2024-08-23 22:14:10 +0200

    Loop over all chromosomes

--- mesoneo_genotypes.sh
+++ mesoneo_genotypes.sh
@@ -1,23 +1,31 @@
 #!/bin/env bash
 
-chrom="21"
+for chrom in $(seq 1 22); do
 
-vcf="/maps/projects/racimolab/data/mesoneo/20200629-impute.1000g.sg_freeze_v1/vcf/20200629-impute.1000g.sg_freeze_v1/${chrom}.neo.impute.1000g.vcf.gz"
+  echo "Processing chromosome $chrom..."
 
-gt="gt_chr${chrom}.tsv"
+  vcf="/maps/projects/racimolab/data/mesoneo/20200629-impute.1000g.sg_freeze_v1/vcf/20200629-impute.1000g.sg_freeze_v1/${chrom}.neo.impute.1000g.vcf.gz"
 
-# write a header line of the genotype table, including sample names
-printf "chrom\tpos\tref\talt\t" > $gt
-bcftools query -l $vcf | tr '\n' '\t' | sed 's/\t$/\n/' >> $gt
+  gt="gt_chr${chrom}.tsv"
 
-# convert the full VCF file into a simple tab-separated table of GTs
-# after first subsetting to only biallelic SNPs
-bcftools view -m2 -M2 -v snps $vcf \
-    | bcftools query -f "%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n" \
-    | sed 's#\.|\.#-#g' \
-    | sed 's#\./\.#-#g' \
-    >> $gt
+  echo "Parsing VCF file '$vcf'..."
 
-Rscript ancestry_sites.R $gt
+  # write a header line of the genotype table, including sample names
+  printf "chrom\tpos\tref\talt\t" > $gt
+  bcftools query -l $vcf | tr '\n' '\t' | sed 's/\t$/\n/' >> $gt
 
-rm $gt
+  # convert the full VCF file into a simple tab-separated table of GTs
+  # after first subsetting to only biallelic SNPs
+  bcftools view -m2 -M2 -v snps $vcf \
+      | bcftools query -f "%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n" \
+      | sed 's#\.|\.#-#g' \
+      | sed 's#\./\.#-#g' \
+      >> $gt
+
+  echo "Filtering for ancestry-informative markers..."
+
+  Rscript ancestry_sites.R $gt
+
+  rm $gt
+
+done
